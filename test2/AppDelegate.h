e771a3ca9b33daa5c057895d9916dfd072f0603a
MuhAsh99 <mjashraf@smu.edu>
2022-09-01 10:09:49 -0500

    Initial Commit

---
+++ test2/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by Muhammad Ashraf on 9/1/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ test2/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by Muhammad Ashraf on 9/1/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
